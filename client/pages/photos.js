c27ee87ee4d357a99977bcce919f0773f84f7f58
yumin-jung <jungym887@naver.com>
2022-05-22 23:22:14 +0900

    Add photos page 📚

--- client/pages/photos.js
+++ client/pages/photos.js
@@ -1,21 +1,29 @@
 import HeadInfo from '../components/HeadInfo'
 import Image from 'next/image'
-// import photosStyles from '../styles/Photos.moudle.css'
+import Link from 'next/link'
+import photosStyles from '../styles/Photos.module.css'
+import testImage from '../data/test.jpg'
+
 
 const photos = ({ photos }) => {
     return (
         <div>
             <HeadInfo title="Photos" />
             <h1>My photos</h1>
-            <ul >
+            <ul calssName={photosStyles.photos}>
                 {photos.map(photo => (
                     <li key={photo.id}>
-                        <Image
-                            src={photo.thumbnailUrl}
-                            width={100}
-                            height={100}
-                            alt={photo.title}
-                        />
+                        <Link href={`/photos/${photo.id}`}>
+                            <a>
+                                <Image
+                                    src={testImage}
+                                    width={100}
+                                    height={100}
+                                    alt={photo.title}
+                                />
+                            </a>
+                        </Link>
+                        <span>{photo.title}</span>
                     </li>
                 ))}
             </ul>
@@ -24,6 +32,18 @@ const photos = ({ photos }) => {
 }
 
 export default photos
+
+// SSR
+// export const getServerSideProps = async () => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_start=0&_end=10`)
+//     const photos = await res.json();
+
+//     return {
+//         props: {
+//             photos
+//         }
+//     }
+// }
 
 // SSG
 export const getStaticProps = async () => {
