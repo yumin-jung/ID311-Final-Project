344a97ccff1b5417db6a6e11dfa98b0511535580
yumin-jung <jungym887@naver.com>
2022-05-24 01:28:48 +0900

    Connect mongodb

--- server/app.js
+++ server/app.js
@@ -1,12 +1,24 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const connection = mongoose.connection;
 const app = express();
 const PORT = 8080;
-const { posts } = require('./data.js')
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect('mongodb+srv://yumin:1234@cluster0.3vbre.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+connection.once("open", () => {
+    console.log("MongoDB database connection succes");
+});
+
 app.get('/api/posts', (req, res) => {
-    res.json(posts)
+    res.json('dkdk')
 })
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`))
