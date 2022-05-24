8320c4746cbf34cf42df8270f2cd4c9545790292
yumin-jung <jungym887@naver.com>
2022-05-24 18:05:58 +0900

    Deploy server with heroku

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
 
 app.use(cors());
