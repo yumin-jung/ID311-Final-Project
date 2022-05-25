3c12c139821281b4c14b881abcde0dbe84377dae
yumin-jung <jungym887@naver.com>
2022-05-25 21:28:50 +0900

    Implement signIn & signUp

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,21 @@
 const router = require("express").Router();
 let User = require("../models/user.model");
 
-router.post("/saveUsers", (req, res) => {
-    console.log(req.body)
+router.post("/saveUser", (req, res) => {
     const user = new User(req.body);
     user.save((err) => {
         if (err) return res.json({ success: false, err });
         else return res.status(200).json({ success: true, user })
     });
 });
+
+router.post('/getUsers', (req, res) => {
+    User.find({})
+        .exec((err, users) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).json({ success: true, users });
+        });
+});
+
 
 module.exports = router;
