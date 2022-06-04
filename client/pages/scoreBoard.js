32b19277d16658806d40fc0655d38d9b47d8fa56
yumin-jung <jungym887@naver.com>
2022-06-04 23:43:49 +0900

    Update scoreboard page responsive

--- client/pages/scoreBoard.js
+++ client/pages/scoreBoard.js
@@ -1,6 +1,6 @@
 import Score from '../components/Score';
 import Message from '../components/Message';
-import StackComponent from '../components/StackComponent';
+import Quiz from '../components/Quiz';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -24,24 +24,24 @@ export default function SignIn() {
                 direction="row"
                 justifyContent="center"
                 alignItems="center"
-                sx={{ marginTop: 10 }}>
+                sx={{ marginTop: 5, display: { xs: 'none', md: 'flex' } }}>
                 <Grid item xs={12}
                     container
                     justifyContent="center"
                     alignItems="center">
                     <Box
                         sx={{
+                            width: '40%',
                             bgcolor: '#f8f8f8',
                             boxShadow: 8,
                             borderRadius: 4,
                             p: 2,
-                            minWidth: 600,
-                            display: 'flex',
+                            minWidth: 400,
                             flexDirection: 'column',
-                            alignItems: 'center'
+                            alignItems: 'center',
                         }}
                     >
-                        <StackComponent />
+                        <Quiz />
                     </Box>
                 </Grid>
                 <Grid item xs={5}
@@ -58,9 +58,9 @@ export default function SignIn() {
                             p: 2,
                             minWidth: 360,
                             marginTop: '5%',
-                            display: 'flex',
                             flexDirection: 'column',
-                            alignItems: 'center'
+                            alignItems: 'center',
+                            display: 'flex'
                         }}
                     >
                         <Typography component="h1" variant="h5">
@@ -89,9 +89,9 @@ export default function SignIn() {
                             p: 2,
                             minWidth: 360,
                             marginTop: '5%',
-                            display: 'flex',
                             flexDirection: 'column',
-                            alignItems: 'center'
+                            alignItems: 'center',
+                            display: 'flex'
                         }}
                     >
                         <Typography component="h1" variant="h5">
@@ -103,6 +103,82 @@ export default function SignIn() {
                             <Message userName="test3" comment="test comment2" />
                             <Message userName="test4" comment="test comment2" />
                             <Message userName="test5" comment="test comment2" />
+                        </List>
+                    </Box>
+                </Grid>
+            </Grid>
+            <Grid container
+                spacing={2}
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                sx={{ marginTop: 4, display: { xs: 'flex', md: 'none' } }}>
+                <Grid item xs={12}
+                    container
+                    justifyContent="center"
+                    alignItems="center">
+                    <Box
+                        sx={{
+                            width: '40%',
+                            bgcolor: '#f8f8f8',
+                            boxShadow: 8,
+                            borderRadius: 4,
+                            p: 2,
+                            minWidth: 300,
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <StackComponent />
+                    </Box>
+                </Grid>
+                <Grid item xs={12}
+                    container
+                    justifyContent="center"
+                    alignItems="center">
+                    <Box sx={{
+                        width: '80%',
+                        bgcolor: '#f8f8f8',
+                        boxShadow: 8,
+                        borderRadius: 4,
+                        p: 2,
+                        minWidth: 360,
+                        marginTop: '5%',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        display: { xs: 'flex', md: 'none' }
+                    }}
+                    >
+                        <Typography component="h1" variant="h5">
+                            Score Board
+                        </Typography>
+                        <List sx={{ width: '80%', maxWidth: 360 }}>
+                            <Score value={1} userName={nick} score={score} quizLength={quizLength} />
+                            <Score value={2} userName="test2" score={9} quizLength={10} />
+                            <Score value={3} userName="test2" score={9} quizLength={10} />
+                        </List>
+                    </Box>
+                    <Box
+                        sx={{
+                            width: '80%',
+                            bgcolor: '#f8f8f8',
+                            boxShadow: 8,
+                            borderRadius: 4,
+                            p: 2,
+                            minWidth: 360,
+                            marginTop: '8%',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            display: { xs: 'flex', md: 'none' }
+                        }}
+                    >
+                        <Typography component="h1" variant="h5">
+                            Messages
+                        </Typography>
+                        <List sx={{ width: '80%', maxWidth: 360 }}>
+                            <Message userName="test1" comment="test comment1" />
+                            <Message userName="test2" comment="test comment2" />
+                            <Message userName="test3" comment="test comment2" />
                         </List>
                     </Box>
                 </Grid>
